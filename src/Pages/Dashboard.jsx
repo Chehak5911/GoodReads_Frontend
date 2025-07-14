950f45c79b98093d648d3401b2e9fde41e9cc7b6
Chehak <chehakbatra5911@gmail.com>
2025-07-14 18:15:50 +0530

    Make book card component

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import BookCard from "Components/BookCard/BookCard";
 import Footer from "Components/Footer/Footer";
 import Navbar from "Components/Navbar/Navbar";
 import Layout from "Layouts/Layout";
@@ -5,7 +6,6 @@ import Layout from "Layouts/Layout";
 export default function Dashboard(){
     return (
         <Layout>
-            <div>Dashboard</div>
         </Layout>
     )
 }
